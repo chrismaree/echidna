e18d3b81c8a5027ff5bff08c03c68063a4d9fa80
Will Song <will.song@trailofbits.com>
2020-01-31 13:04:17 -0500

    zip

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -16,7 +16,7 @@ if [ "$HOST_OS" = "Linux" ]; then
   curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_linux_amd64.tar.gz" > ghr.tar.gz
   tar -C "$HOME/.local/bin" -xf ghr.tar.gz
 elif [ "$HOST_OS" = "macOS" ]; then
-  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_darwin_amd64.tar.gz" > ghr.zip
+  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_darwin_amd64.zip" > ghr.zip
   unzip ghr.zip -d "$HOME/.local/bin"
 else
   echo "unknown host"
