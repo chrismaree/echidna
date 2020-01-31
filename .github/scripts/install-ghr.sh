f8546953307c430fac591b607b7b4bc016ff6868
Will Song <will.song@trailofbits.com>
2020-01-31 13:01:10 -0500

    tar.gz

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -7,19 +7,18 @@ if [ -f $HOME/.local/bin/ghr ]; then
   exit 0
 fi
 
+VERSION="v0.13.0"
+GITHUB="https://github.com/tcnksm/ghr/releases/download"
+
+echo "Installing ghr"
+mkdir -p "$HOME/.local/bin"
 if [ "$HOST_OS" = "Linux" ]; then
-  ARCH="linux"
+  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_linux_amd64.tar.gz" > ghr.tar.gz
+  tar -C "$HOME/.local/bin" -xf ghr.tar.gz
 elif [ "$HOST_OS" = "macOS" ]; then
-  ARCH="darwin"
+  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_darwin_amd64.tar.gz" > ghr.zip
+  unzip ghr.zip -d "$HOME/.local/bin"
 else
   echo "unknown host"
   exit 1
 fi
-
-VERSION="v0.13.0"
-
-echo "Installing ghr"
-URL="https://github.com/tcnksm/ghr/releases/download/${VERSION}/ghr_${VERSION}_${ARCH}_amd64.zip"
-curl -L ${URL} > ghr.zip
-mkdir -p "$HOME/.local/bin"
-unzip ghr.zip -d "$HOME/.local/bin"
