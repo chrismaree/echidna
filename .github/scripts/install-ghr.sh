4cd62c4657fcdfe11cf2f8d595e381fbafb84701
Will Song <will.song@trailofbits.com>
2020-01-31 15:17:26 -0500

    fix bincache

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -2,23 +2,28 @@
 
 set -ex
 
-if [ -f $HOME/.local/bin/ghr ]; then
+if [ -f "$HOME/.local/bin/ghr" ]; then
   echo "found ghr, exiting..."
   exit 0
 fi
 
-VERSION="v0.13.0"
 GITHUB="https://github.com/tcnksm/ghr/releases/download"
+VERSION="v0.13.0"
 
 echo "Installing ghr"
 mkdir -p "$HOME/.local/bin"
 if [ "$HOST_OS" = "Linux" ]; then
-  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_linux_amd64.tar.gz" > ghr.tar.gz
-  tar -C "$HOME/.local/bin" -xf ghr.tar.gz
+  ARCH="linux"
+  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_${ARCH}_amd64.tar.gz" > ghr.tar.gz
+  tar -xf ghr.tar.gz
 elif [ "$HOST_OS" = "macOS" ]; then
-  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_darwin_amd64.zip" > ghr.zip
-  unzip ghr.zip -d "$HOME/.local/bin"
+  ARCH="darwin"
+  curl -L "${GITHUB}/${VERSION}/ghr_${VERSION}_${ARCH}_amd64.zip" > ghr.zip
+  unzip ghr.zip
 else
   echo "unknown host"
   exit 1
 fi
+
+DIRNAME="ghr_${VERSION}_${ARCH}_amd64"
+mv "${DIRNAME}/ghr" "$HOME/.local/bin/ghr"
