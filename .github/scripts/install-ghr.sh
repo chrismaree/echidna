83616220a1324a8ff768b3980b604203cebb840b
Will Song <will.song@trailofbits.com>
2020-01-31 12:32:22 -0500

    fix cache bins

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -11,7 +11,7 @@ elif [ "$HOST_OS" = "macOS" ]; then
 fi
 
 echo "Installing ghr"
-URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_${ARCH}_386.zip"
+URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_${ARCH}_amd64.zip"
 curl -L ${URL} > ghr.zip
 mkdir -p "$HOME/.local/bin"
 unzip ghr.zip -d "$HOME/.local/bin"
