8ac1aaa67397c6762cdf9dbfc9d3131ac958517e
Will Song <will.song@trailofbits.com>
2020-01-31 12:54:47 -0500

    oops

--- .github/scripts/install-ghr.sh
+++ .github/scripts/install-ghr.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
+set -ex
+
 if [ -f $HOME/.local/bin/ghr ]; then
+  echo "found ghr, exiting..."
   exit 0
 fi
 
@@ -8,10 +11,15 @@ if [ "$HOST_OS" = "Linux" ]; then
   ARCH="linux"
 elif [ "$HOST_OS" = "macOS" ]; then
   ARCH="darwin"
+else
+  echo "unknown host"
+  exit 1
 fi
 
+VERSION="v0.13.0"
+
 echo "Installing ghr"
-URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_${ARCH}_amd64.zip"
+URL="https://github.com/tcnksm/ghr/releases/download/${VERSION}/ghr_${VERSION}_${ARCH}_amd64.zip"
 curl -L ${URL} > ghr.zip
 mkdir -p "$HOME/.local/bin"
 unzip ghr.zip -d "$HOME/.local/bin"
